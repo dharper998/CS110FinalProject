a492c44b3e7532e2aedd628a947af40f5b67e262
Drew Harper <31670344+dharper998@users.noreply.github.com>
2017-11-03 10:37:37 -0400

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,24 +1,31 @@
 def main():
 	print("#testing driver car model#")
 	test_driver=driver.Driver()
+	
 	print("#standard input test#")
 	test_driver.moveright(3)
 	assert test_driver.getcoordinates()==(3,0)
+	
 	print('#zero input test#')
 	test_driver.moveright(0)
 	assert test_driver.getcoordinates()==(3,0)
+	
 	print('#negative input test#')
 	test_driver.moveright(-1)
 	assert test_driver.getcoordinates()==(2,0)
+	
 	print("#testing traffic model#")
 	test_traffic=driver.Traffic()
+	
 	print('#standard input test#')
 	test_traffic.speedup(2)
-	assert test_traffic.speddup()==(0,2)
+	assert test_traffic.speedup()==(0,2)
+	
 	print('#zero input test#')
 	test_traffic.speedup(0)
-	assert test_traffic.speddup()==(0,2)
+	assert test_traffic.speedup()==(0,2)
+	
 	print('#negative input test#')
 	test_traffic.speedup(-1)
-	assert test_traffic.speddup()==(0,1)
+	assert test_traffic.speedup()==(0,1)
 main()
