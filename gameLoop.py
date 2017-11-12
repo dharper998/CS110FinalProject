72fef96d1aec6b5c477ede61dfac58d61e688f55
dharper998 <dharper998@gmail.com>
2017-11-12 15:43:36 -0500

    Added comments

--- gameLoop.py
+++ gameLoop.py
@@ -8,11 +8,15 @@ class GameLoop:
     def __init__(self, gamedisplay, drivercar):
         self.gamedisplay = gamedisplay
         self.drivercar = drivercar
+        
     def loop(self):
         while True:
+            #checks incoming events like key presses
             event = pygame.event.poll()
             if event.type == pygame.QUIT:
                 break
+
+            #sets background to white (will be replaced by road) and updates the screen to include the driver car
             self.gamedisplay.fill((255, 255, 255))
             self.gamedisplay.blit(self.drivercar.driverimg, (self.drivercar.x, self.drivercar.y))
             pygame.display.flip()
--- main.py
+++ main.py
@@ -8,10 +8,15 @@ import driver
 #import traffic
 
 def main():
+    #create game object and run setup method
     game = gameSetup.GameSetup()
     game.setup()
+
+    #create seperate variables for gamedisplay and drivercar to use as parameters
     gamedisplay = game.gamedisplay
     drivercar = driver.Driver(200, 200)
+
+    #create loop object and run loop method
     loop = gameLoop.GameLoop(gamedisplay, drivercar)
     loop.loop()
 main()
