0d0fe644a7e14f56812832b5be7d82d10b80d5a2
dharper998 <dharper998@gmail.com>
2017-11-14 23:36:32 -0500

    More work on traffic in lanes

--- main.py
+++ main.py
@@ -15,8 +15,12 @@ def main():
     #Creates seperate variable for display and creates drivercar object
     gamedisplay = game.gamedisplay
     drivercar = driver.Driver(500, 300)
+    lane1 = traffic.Traffic(400)
+    lane2 = traffic.Traffic(500)
+    lane3 = traffic.Traffic(600)
+    lane4 = traffic.Traffic(700)
 
     #Creates a loop object and runs the loop method
-    gameloop = gameLoop.GameLoop(gamedisplay, drivercar)
+    gameloop = gameLoop.GameLoop(gamedisplay, drivercar, lane1)
     gameloop.loop()
 main()
--- traffic.py
+++ traffic.py
@@ -1,22 +1,10 @@
 import random
 
 class Traffic(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, x):
         pygame.sprite.Sprite.__init__(self)
+        self.x = x
         self.speeds = [1, 2, 3, 4, 5]
         self.images = ["Black_viper.png"]
         self.trafficimage = pygame.image.load("Black_viper.png")
         self.rect = self.trafficimage.get_rect()
-    def setuploop(self):
-
-    def lane1(self):
-
-
-    def lane2(self):
-
-
-    def lane3(self):
-
-
-    def lan4(self):
-        
